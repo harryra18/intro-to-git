5b8aeab145400609d3924a0416bd0f861cacb8c6
harryra18 <harryra18@gmail.com>
2025-06-07 15:54:23 -0400

    MVDP Fandom Homework

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Hello Workdl")
+console.log("Hello Workdl"); lightMode = document.querySelector("#light-mode");
+const darkMode = document.querySelector("#dark-mode");
+const body = document.querySelector("body");
+
+lightMode.addEventListener("click", function() {
+  body.style.backgroundColor = "#FFF3EB"
+});
+darkMode.addEventListener("click", function() {
+  body.style.backgroundColor = "#A54800"
+})
+
+const mainSection = document.querySelector("#main");
+mainSection.style.color = "blue"
+
+
+
+
+
+
+
+
+
+
+
+
